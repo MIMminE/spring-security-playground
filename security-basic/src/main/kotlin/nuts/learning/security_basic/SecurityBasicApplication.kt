e133f21da637ab72e7a8414304edd0ac9d391a33
MIMminE <weluv.ww@gmail.com>
2025-10-24 16:53:00 +0900

    first commit

---
+++ security-basic/src/main/kotlin/nuts/learning/security_basic/SecurityBasicApplication.kt
@@ -0,0 +1,11 @@
+package nuts.learning.security_basic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecurityBasicApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecurityBasicApplication>(*args)
+}
